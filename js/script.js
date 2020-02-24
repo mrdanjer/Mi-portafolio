7198a41bc112a752a4c94209a5487c92c028aec3
mrdanjer <danilo.araneda.vega@gmail.com>
2020-02-23 22:25:57 -0300

    Mi portafolio completado

--- js/script.js
+++ js/script.js
@@ -2,22 +2,20 @@ $(document).ready(function(){
     $(a).on('click', function(event) {
       if (this.hash !== "") {
         event.preventDefault();
-  
         var hash = this.hash;
-  
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
         $('html, body').animate({
           scrollTop: $(hash).offset().top
         },200, function(){
-  
-          // Add hash (#) to URL when done scrolling (default click behavior)
           window.location.hash = hash;
         });
-      } // End if
+      }
     });
+
     $(function () {
       $('[data-toggle="tooltip"]').tooltip()
-    })
+    });
 
-  });
+    // $("#myBtn").click(function() {
+    //   $("#myModal").modal(show);
+    // });
+});
